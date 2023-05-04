7ee29c6b15c9e3ee18636427e5478ea606978833
diallodeveloper <diallocodeur@gmail.com>
2023-05-04 15:10:08 +0000

    intialize inventory service and define the model class

---
+++ inventory-service/src/main/java/com/diallodev/inventoryservice/inventory/Inventory.java
@@ -0,0 +1,9 @@
+package com.diallodev.inventoryservice.inventory;
+
+public class Inventory {
+    private Long id;
+    private String skuCode;
+    private Integer quantity;
+
+
+}
---
+++ inventory-service/src/main/java/com/diallodev/inventoryservice/inventory/InventoryRepository.java
@@ -0,0 +1,4 @@
+package com.diallodev.inventoryservice.inventory;
+
+public class InventoryRepository {
+}
