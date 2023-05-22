72d1e47c4b4d8faa32f975be11d3bfbdbc9de67a
diallodeveloper <diallocodeur@gmail.com>
2023-05-22 07:27:11 +0000

    update code

--- discovery-server/src/main/java/com/dialldev/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/dialldev/discoveryserver/DiscoveryServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServergit
+@EnableEurekaServer
 public class DiscoveryServerApplication {
 
     public static void main(String[] args) {
