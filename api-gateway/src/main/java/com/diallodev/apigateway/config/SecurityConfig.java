1a05849063e8d6c74b56428b029d49835767483e
diallodeveloper <diallocodeur@gmail.com>
2023-05-24 09:18:26 +0000

    add keycloak security configuration with api-gateway service

--- api-gateway/src/main/java/com/diallodev/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/diallodev/apigateway/config/SecurityConfig.java
@@ -1,2 +1,29 @@
-package com.diallodev.apigateway.config;public class SecurityConfig {
+package com.diallodev.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
+        http.csrf().disable()
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/eureka/**")
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated()
+                        .and()
+                        .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt));
+        return http.build();
+
+    }
+
+
 }
